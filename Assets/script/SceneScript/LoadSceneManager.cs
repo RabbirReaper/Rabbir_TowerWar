e15bca60935c92b614016b796514458bc57406c6
RabbirReaper <abc0979999412@gmail.com>
2023-08-18 15:18:47 +0800

    add

--- Assets/script/SceneScript/LoadSceneManager.cs
+++ Assets/script/SceneScript/LoadSceneManager.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Photon.Pun;
-using Photon.Realtime;
+using UnityEngine.SceneManagement;
 public class LoadSceneManager : MonoBehaviourPunCallbacks{
     [SerializeField] TMP_Text roomCurrentPlayerText;
 
     private void Start() {
         roomCurrentPlayerText.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
     }
-    public override void OnJoinedRoom(){
+    private void Update() {
         roomCurrentPlayerText.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 2){
+            SceneManager.LoadScene("SampleScene");
+        }
+        
     }
+    // public override void OnJoinedRoom(){
+    //     roomCurrentPlayerText.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
+    //     if(2 == PhotonNetwork.CurrentRoom.PlayerCount){
+
+    //     }
+    // }
+    // public override void OnLeftRoom(){
+    //     roomCurrentPlayerText.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
+    // }
 
 }
