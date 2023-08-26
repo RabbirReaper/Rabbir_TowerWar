82b4eba13ec25133702a699eebe5542561389ad3
RabbirReaper <abc0979999412@gmail.com>
2023-08-26 23:02:49 +0800

    Archer OK

--- Assets/script/UpGradeUpdateTower.cs
+++ Assets/script/UpGradeUpdateTower.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UpGradeUpdateTower : MonoBehaviour{
     [SerializeField] GameObject nextLevelTower;
+    [SerializeField] GameObject nextLevelTower2;
     [SerializeField] GameObject upgradeUI;
     [SerializeField] int level;
     [SerializeField] int baseUpGradeCost;
@@ -14,10 +15,18 @@ public class UpGradeUpdateTower : MonoBehaviour{
         upgradeUI.SetActive(false);
     }
     public void Upgrade(){
-        if(level >= 3) return;
+        if(level >= 4) return;
         if(baseUpGradeCost > LevelManager_script.main.Gold) return;
         LevelManager_script.main.SpendCurrency(baseUpGradeCost);
         GetComponentInParent<Plot>().TowerUpdate(nextLevelTower);
+        UIManager.main.SetHoveringStatie(false);
+        Destroy(gameObject);
+    }
+    public void Upgrade2(){
+        if(level >= 4) return;
+        if(baseUpGradeCost > LevelManager_script.main.Gold) return;
+        LevelManager_script.main.SpendCurrency(baseUpGradeCost);
+        GetComponentInParent<Plot>().TowerUpdate(nextLevelTower2);
         UIManager.main.SetHoveringStatie(false);
         Destroy(gameObject);
     }
