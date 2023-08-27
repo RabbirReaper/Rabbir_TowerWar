6379085f67672926612f3501b7de483296cca64e
RabbirReaper <abc0979999412@gmail.com>
2023-08-27 16:43:05 +0800

    temp

---
+++ Assets/script/GameSceneUI/SellingTooltip.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class SellingTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+    [SerializeField] Image imagePrefab; // 需要在 Inspector 中設定預製體
+    [SerializeField] GameObject tower;
+    [SerializeField] Canvas canvas;
+
+    private Image spawnedImage;
+    private TMP_Text text;
+
+    private void Update(){
+        if (spawnedImage != null){
+            spawnedImage.transform.position = new Vector3(Input.mousePosition.x + spawnedImage.rectTransform.rect.width*0.5f + 0.1f,Input.mousePosition.y - spawnedImage.rectTransform.rect.height*0.5f-0.1f,0);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        spawnedImage = Instantiate(imagePrefab);
+        text = spawnedImage.GetComponentInChildren<TMP_Text>();
+        spawnedImage.transform.SetParent(transform); // 設定父物件
+        // spawnedImage.rectTransform.sizeDelta = new Vector2(50, 50); // 設定大小
+        Turret towerScript = tower.GetComponent<Turret>();
+        text.text = tower.name + "\n\nSelling: " + towerScript.sellValue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        // 刪除 UI
+        if (spawnedImage != null){
+            Destroy(spawnedImage.gameObject);
+            spawnedImage = null;
+        }
+    }
+}
