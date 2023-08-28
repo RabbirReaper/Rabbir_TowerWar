cb43c07bdc58585fde0252a9fb9409653bcfc402
RabbirReaper <abc0979999412@gmail.com>
2023-08-28 09:03:30 +0800

    ArcherUpButton Script OK

---
+++ Assets/script/GameSceneUI/ArcherUpButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ArcherUpButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+    [SerializeField] string _name;
+    [SerializeField] GameObject tower;
+    [SerializeField] int cost;
+    private string str;
+
+    private void Start() {
+        Turret towerScript = tower.GetComponent<Turret>();
+        str = "\n " + _name +"\n\n\n\n Damage:  " + towerScript.bulletPrefab.GetComponent<Bullet>().Bullet_Damage.ToString() +"\n\n Reload:  " + towerScript.reload + "\n\n AttachRange:  " + towerScript.AttackRange + "\n\n\n\n Cost: " + cost;
+    }
+    public void OnPointerEnter(PointerEventData eventData){
+        TooltipScreen.main.SetActive(true);
+        TooltipScreen.main.SetText(str);
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        TooltipScreen.main.SetActive(false);
+    }
+}
