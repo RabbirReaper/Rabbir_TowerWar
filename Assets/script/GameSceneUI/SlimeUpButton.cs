06ff776615f708de25bd75a4a00f654ef0fc2e4a
RabbirReaper <abc0979999412@gmail.com>
2023-08-28 09:13:00 +0800

    SlimeUpButton Script OK

--- Assets/script/GameSceneUI/SlimeUpButton.cs
+++ Assets/script/GameSceneUI/SlimeUpButton.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ArcherUpButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+public class SlimeUpButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
     [SerializeField] string _name;
     [SerializeField] GameObject tower;
     [SerializeField] int cost;
     private string str;
-
     private void Start() {
-        Turret towerScript = tower.GetComponent<Turret>();
-        str = "\n " + _name +"\n\n\n\n Damage:  " + towerScript.bulletPrefab.GetComponent<Bullet>().Bullet_Damage.ToString() +"\n\n Reload:  " + towerScript.reload + "\n\n AttachRange:  " + towerScript.AttackRange + "\n\n\n\n Cost: " + cost;
+        Slime_Turret towerScript = tower.GetComponent<Slime_Turret>();
+        str ="\n " + _name +"\n\n\n\n Damage:  " + towerScript.bulletPrefab.GetComponent<Slime_Bullet>().Bullet_Damage +"\n\n Reload:  " + towerScript.reload + "\n\n AttachRange:  " + towerScript.AttackRange + "\n\n Splash: " +towerScript.bulletPrefab.GetComponent<Slime_Bullet>().slowRange +"\n\n\n\n Cost: " + cost;
     }
     public void OnPointerEnter(PointerEventData eventData){
         TooltipScreen.main.SetActive(true);
