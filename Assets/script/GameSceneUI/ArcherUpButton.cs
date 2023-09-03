4cf9ac1a4b2fde39bfd1d047d42bebc6df49f9fc
RabbirReaper <abc0979999412@gmail.com>
2023-09-03 20:14:22 +0800

    Sniper Set OK

--- Assets/script/GameSceneUI/ArcherUpButton.cs
+++ Assets/script/GameSceneUI/ArcherUpButton.cs
@@ -12,9 +12,16 @@ public class ArcherUpButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
     private void Start() {
         Turret towerScript = tower.GetComponent<Turret>();
-        str = "\n " + _name +"\n\n\n\n Damage:  " + towerScript.bulletPrefab.GetComponent<Bullet>().Bullet_Damage.ToString() +"\n\n Reload:  " + towerScript.reload + "\n\n AttachRange:  " + towerScript.AttackRange ;
-        if(special != "null") str +="\n\n\n\n Special:  "+ special ;
-        str +="\n\n\n\n Cost: " + cost;
+        if(towerScript == null){
+            Sniper sniper = tower.GetComponent<Sniper>();
+            str = "\n " + _name +"\n\n\n\n Damage:  " + sniper.bulletPrefab.GetComponent<Bullet>().Bullet_Damage.ToString() +"\n\n Reload:  " + sniper.reload + "\n\n AttachRange:  " + sniper.AttackRange ;
+            if(special != "null") str +="\n\n\n\n Special:  "+ special ;
+            str +="\n\n\n\n Cost: " + cost;
+        }else{
+            str = "\n " + _name +"\n\n\n\n Damage:  " + towerScript.bulletPrefab.GetComponent<Bullet>().Bullet_Damage.ToString() +"\n\n Reload:  " + towerScript.reload + "\n\n AttachRange:  " + towerScript.AttackRange ;
+            if(special != "null") str +="\n\n\n\n Special:  "+ special ;
+            str +="\n\n\n\n Cost: " + cost;
+        }
     }
     public void OnPointerEnter(PointerEventData eventData){
         TooltipScreen.main.SetActive(true);
---
+++ Assets/script/TowerAndBullet/Sniper.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class Sniper : MonoBehaviour{
+
+    [SerializeField] GameObject barrel;
+    public GameObject bulletPrefab;
+    LayerMask EnemyMask;
+
+    LayerMask shieldMask;
+
+    
+    [SerializeField] Transform firingPoint;
+    public float AttackRange;
+    [SerializeField] float RotationSpeed;
+    public float reload;
+    public int sellValue;
+    Transform target;
+    float timeUntilFire=0;
+    [SerializeField] GameObject slowImage;
+    [SerializeField] GameObject brokenImage;
+    int slowCount = 0;
+    float slowRate;
+    int brokenCount = 0;
+    float buff = 1;
+
+    private void Start() {
+        EnemyMask = LayerMask.GetMask("Enemy","Ghost");
+        shieldMask = LayerMask.GetMask("Shield");
+    }
+    private void Update() {
+        timeUntilFire -= Time.deltaTime;
+        if(target == null){
+            FindTarget();
+            return;
+        }
+        RotateTowards();
+        if(!CheckTargetinRange()){
+            target=null;
+        }else{
+            if(timeUntilFire <= 0){
+                if(brokenCount == 0) Shoot();
+                else{
+                    Debug.Log("Is Broken");
+                    brokenCount--;
+                    if(brokenCount == 0) brokenImage.SetActive(false);
+                } 
+                if(ShieldL2InRange()){
+                    timeUntilFire= reload*0.9f;
+                }else if(slowCount != 0){
+                    slowCount--;
+                    timeUntilFire = reload*(1 + slowRate);
+                    if(slowCount == 0) slowImage.SetActive(false);
+                }else timeUntilFire=reload;
+                Debug.Log(timeUntilFire);
+            }
+        }
+    }
+
+    void Shoot(){
+        GameObject bulletobj = Instantiate(bulletPrefab,firingPoint.position,Quaternion.identity);
+        Bullet bulletScript = bulletobj.GetComponent<Bullet>();
+        bulletScript.Bullet_Damage*=buff;
+        bulletScript.SetTarget(target);
+        buff = 1;
+    }
+
+    bool CheckTargetinRange(){
+        return Vector2.Distance(target.position,transform.position) <= AttackRange;
+    }
+
+    void FindTarget(){
+        // RaycastHit2D hits = Physics2D.CircleCast(transform.position,AttackRange,transform.position,0f,EnemyMask);
+        // target = hits.transform;
+        Collider2D[] inRange = Physics2D.OverlapCircleAll(transform.position,AttackRange,EnemyMask);
+        if(inRange.Length != 0) target = inRange[0].transform;
+        for(int i=0;i<(int)inRange.Length;i++){
+            if(inRange[i] == null) continue;
+            if(inRange[i].GetComponent<Enemy_Script>().GetNowHealth() / inRange[i].GetComponent<Enemy_Script>().Health > 0.5){
+                buff = 2;
+                target = inRange[i].transform;
+                return;
+            }
+        }
+    }
+
+    void RotateTowards(){
+        float angle = Mathf.Atan2(target.position.y - barrel.transform.position.y,target.position.x - barrel.transform.position.x)*Mathf.Rad2Deg-90f;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f,0f,angle));
+        barrel.transform.rotation = Quaternion.RotateTowards(barrel.transform.rotation,targetRotation,RotationSpeed*Time.deltaTime);
+    }
+
+    public void SellingTower(){
+        LevelManager_script.main.IncreaseGold(sellValue);
+        UIManager.main.SetHoveringStatie(false);
+        LevelManager_script.main.towerCountLimit++;
+        Destroy(this.gameObject);
+    }
+
+    bool ShieldL2InRange(){
+        Collider2D[] inRange = Physics2D.OverlapCircleAll(transform.position,10,shieldMask);
+        foreach (var item in inRange){
+            if(item.GetComponent<Villager_Turret>().level == 2) return true;
+            Debug.Log(item.GetComponent<Villager_Turret>().level + "!!!");
+        }
+        return false;
+    } 
+    bool ShieldL1InRange(){
+        Collider2D inRange = Physics2D.OverlapCircle(transform.position,5,shieldMask);
+        if(inRange == null) return false;
+        return true;
+    }
+    public void SlowTurret(float _slowRate,int _slowCount){
+        if(ShieldL1InRange() || ShieldL2InRange()) return;
+        Debug.Log(" I slow");
+        slowImage.SetActive(true);
+        slowCount = _slowCount;
+        slowRate = _slowRate;
+    }
+
+    public void UpdateIsborken(int _brokenCount){
+        if(ShieldL2InRange()) return;
+        brokenImage.SetActive(true);
+        brokenCount = _brokenCount;
+    }
+    // private void OnDrawGizmosSelected() {
+    //     Handles.color=Color.blue;
+    //     Handles.DrawWireDisc(transform.position,transform.forward,AttackRange);
+    // }
+
+}
